085f01d12a51dde489e2b4e686ae2c9a3a9b927e
sarbajitdutta <sarbajitdutta35@gmail.com>
2019-03-13 13:55:06 -0400

    add queryType and remove some unnecessary properties

--- src/reciter/model/identity/Identity.java
+++ src/reciter/model/identity/Identity.java
@@ -20,8 +20,6 @@ package reciter.model.identity;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDocument;
 
-import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 
 @DynamoDBDocument
@@ -38,15 +36,17 @@ public class Identity {
 	private Education degreeYear; // degreeYear
 	private List<String> personTypes; // type of person: i.e., academic, academic-faculty, etc...
 	private String program; // program the person is in.
-	private List<String> boardCertifications; // board certifications
-	private String citizenship; // citizenship
-	private List<String> grants; // grants
-	private List<String> keywords; // keywords
-	private List<PubMedAlias> pubMedAlias; // name alias from PubMed
-	private Date dateInitialRun; // the date of the first time that ReCiter perform the retrieval
-	private Date dateLastRun; // the date of the most recent retrieval
+	private List<String> grants; // grantsl
+	private QueryType queryType;
 	
 	public Identity() {}
+	
+	public enum QueryType {
+		LENIENT_LOOKUP,
+		STRICT_COMPOUND_NAME_LOOKUP,
+		STRICT_EXCEEDS_THRESHOLD_LOOKUP
+		
+	}
 
 	public String getUid() {
 		return uid;
@@ -136,22 +136,6 @@ public class Identity {
 		this.program = program;
 	}
 
-	public List<String> getBoardCertifications() {
-		return boardCertifications;
-	}
-
-	public void setBoardCertifications(List<String> boardCertifications) {
-		this.boardCertifications = boardCertifications;
-	}
-
-	public String getCitizenship() {
-		return citizenship;
-	}
-
-	public void setCitizenship(String citizenship) {
-		this.citizenship = citizenship;
-	}
-
 	public List<String> getGrants() {
 		return grants;
 	}
@@ -160,39 +144,12 @@ public class Identity {
 		this.grants = grants;
 	}
 
-	public List<String> getKeywords() {
-		return keywords;
+	public QueryType getQueryType() {
+		return queryType;
 	}
 
-	public void setKeywords(List<String> keywords) {
-		this.keywords = keywords;
+	public void setQueryType(QueryType queryType) {
+		this.queryType = queryType;
 	}
-
-	public List<PubMedAlias> getPubMedAlias() {
-		if (pubMedAlias != null) {
-			return pubMedAlias;
-		} else {
-			return Collections.emptyList();
-		}
-	}
-
-	public void setPubMedAlias(List<PubMedAlias> pubMedAlias) {
-		this.pubMedAlias = pubMedAlias;
-	}
-
-	public Date getDateInitialRun() {
-		return dateInitialRun;
-	}
-
-	public void setDateInitialRun(Date dateInitialRun) {
-		this.dateInitialRun = dateInitialRun;
-	}
-
-	public Date getDateLastRun() {
-		return dateLastRun;
-	}
-
-	public void setDateLastRun(Date dateLastRun) {
-		this.dateLastRun = dateLastRun;
-	}
+	
 }
