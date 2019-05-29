bd27b796a8bc173120638075b785f28c46af6023
sarbajitdutta <sarbajitdutta35@gmail.com>
2019-05-29 14:03:39 -0400

    added sanitization for institutions

--- src/reciter/model/identity/Identity.java
+++ src/reciter/model/identity/Identity.java
@@ -22,9 +22,17 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDocument;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import lombok.Data;
+
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+/**
+ * @author szd2013
+ * This class contains defines the Identity which will be used to suggest Publications by ReCiter
+ */
+@Data
 @DynamoDBDocument
 public class Identity {
 
@@ -44,118 +52,10 @@ public class Identity {
 	@DynamoDBIgnore
 	@JsonIgnore
 	private Map<AuthorName, AuthorName> sanitizedNames; //This contains sanitized names of both primary and alternate names
-	
-	public Identity() {}
-
-	public String getUid() {
-		return uid;
-	}
-
-	public void setUid(String uid) {
-		this.uid = uid;
-	}
-
-	public AuthorName getPrimaryName() {
-		return primaryName;
-	}
-
-	public void setPrimaryName(AuthorName primaryName) {
-		this.primaryName = primaryName;
-	}
-
-	public List<AuthorName> getAlternateNames() {
-		return alternateNames;
-	}
-
-	public void setAlternateNames(List<AuthorName> alternateNames) {
-		this.alternateNames = alternateNames;
-	}
-
-	public List<String> getEmails() {
-		return emails;
-	}
-
-	public void setEmails(List<String> emails) {
-		this.emails = emails;
-	}
-
-	public List<KnownRelationship> getKnownRelationships() {
-		return knownRelationships;
-	}
-
-	public void setKnownRelationships(List<KnownRelationship> knownRelationships) {
-		this.knownRelationships = knownRelationships;
-	}
-
-	public List<OrganizationalUnit> getOrganizationalUnits() {
-		return organizationalUnits;
-	}
-
-	public void setOrganizationalUnits(List<OrganizationalUnit> organizationalUnits) {
-		this.organizationalUnits = organizationalUnits;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public List<String> getInstitutions() {
-		return institutions;
-	}
-
-	public void setInstitutions(List<String> institutions) {
-		this.institutions = institutions;
-	}
-
-	public Education getDegreeYear() {
-		return degreeYear;
-	}
-
-	public void setDegreeYear(Education degreeYear) {
-		this.degreeYear = degreeYear;
-	}
-
-	public List<String> getPersonTypes() {
-		return personTypes;
-	}
-
-	public void setPersonTypes(List<String> personTypes) {
-		this.personTypes = personTypes;
-	}
-
-	public String getProgram() {
-		return program;
-	}
-
-	public void setProgram(String program) {
-		this.program = program;
-	}
-
-	public List<String> getGrants() {
-		return grants;
-	}
-
-	public void setGrants(List<String> grants) {
-		this.grants = grants;
-	}
-
-	public List<PubMedAlias> getPubMedAlias() {
-		return pubMedAlias;
-	}
-
-	public void setPubMedAlias(List<PubMedAlias> pubMedAlias) {
-		this.pubMedAlias = pubMedAlias;
-	}
-
-	public Map<AuthorName, AuthorName> getSanitizedNames() {
-		return sanitizedNames;
-	}
-
-	public void setSanitizedNames(Map<AuthorName, AuthorName> sanitizedNames) {
-		this.sanitizedNames = sanitizedNames;
-	}
+	@DynamoDBIgnore
+	@JsonIgnore
+	private Set<OrganizationalUnit> sanitizedIdentityInstitutions; // sanitized Map of Institutions also will contain synonyms for orgUnit as well
+	@DynamoDBIgnore
+	@JsonIgnore
+	private Map<String, List<String>> identityOrgUnitToSynonymMap; // Map for a actual orgUnit to synonym as defined by ReCiter application
 }
